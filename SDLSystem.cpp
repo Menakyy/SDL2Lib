5e8902c512d0bfce5ed4285a7c87254d75d87f7d
konrad.ryszawy <konrad.ryszawy@techsterowniki.pl>
2025-03-11 09:03:24 +0100

    Ability to initialize an individual sdl module

--- SDLSystem.cpp
+++ SDLSystem.cpp
@@ -2,21 +2,48 @@
 
 #include "Logger.h"
 
-SDLSystem::SDLSystem(Uint32 sdlInitFlags) : sdlInitFlags(sdlInitFlags)
+SDLSystem::SDLSystem(Uint32 sdlInitFlags, InitOptions initOptions, AudioParameters audioParameters)
+    : sdlInitFlags(sdlInitFlags),
+      audioParameters(audioParameters),
+      initOptions(initOptions)
 {
+    bool initSuccess = true;
+
     if (SDL_Init(sdlInitFlags) != 0)
     {
         Logger::error(("SDL_Init Error: " + std::string(SDL_GetError())).c_str());
+        initSuccess = false;
     }
 
-    if (TTF_Init() == -1)
+    if ((initOptions & ttf) != 0 && TTF_Init() == -1)
     {
         Logger::error(("TTF_Init Error: " + std::string(TTF_GetError())).c_str());
+        initSuccess = false;
     }
+
+    int mixInitCode = Mix_OpenAudio(audioParameters.frequency,
+                                       audioParameters.format,
+                                       audioParameters.channels,
+                                       audioParameters.chunksize);
+
+    if ((initOptions & audio) != 0 && mixInitCode == -1)
+    {
+        Logger::error(("Mix_OpenAudio Error: " + std::string(Mix_GetError())).c_str());
+        initSuccess = false;
+    }
+
+    if (not initSuccess)
+    {
+        Logger::error("SDLSystem initialization failed");
+        std::abort();
+    }
+
+    Logger::info("SDLSystem initialized");
 }
 
 SDLSystem::~SDLSystem()
 {
+    Mix_CloseAudio();
     TTF_Quit();
     SDL_Quit();
 }
