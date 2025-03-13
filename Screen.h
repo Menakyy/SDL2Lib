201c7f84b3a64738536742b6ca1a19c2cda44c70
konrad.ryszawy <konrad2223@gmail.com>
2025-03-13 23:18:10 +0100

    Added screen class

---
+++ Screen.h
@@ -0,0 +1,18 @@
+#ifndef SCREEN_H
+#define SCREEN_H
+
+#include "Container.h"
+#include "EventHandler.h"
+#include "Renderer.h"
+
+class Screen
+{
+public:
+    virtual ~Screen() = default;
+
+    virtual void handleEvents(EventHandler& eventHandler, bool& exit) = 0;
+    virtual void update()                                             = 0;
+    virtual void render(Renderer& renderer)                           = 0;
+};
+
+#endif  // SCREEN_H
