45e45e318a5a88cac71e47daea072599525a34b2
konrad.ryszawy <konrad2223@gmail.com>
2025-03-30 22:58:57 +0200

    Button class triggers callback

--- Button.cpp
+++ Button.cpp
@@ -20,13 +20,13 @@ bool Button::isWidgetClicked(EventHandler& eventHandler)
     Point    mousePos   = Utilities::getMousePosition(eventHandler.getEvent());
     SDL_Rect widgetRect = Utilities::convertToSDLRect(position, size);
 
-    Logger::debug(
-        ("Mouse Position: (" + std::to_string(mousePos.getX()) + ", " + std::to_string(mousePos.getY()) + ")").c_str());
-    Logger::debug(("Button Area: (" + std::to_string(widgetRect.x) + ", " + std::to_string(widgetRect.y) + ", "
-                   + std::to_string(widgetRect.w) + ", " + std::to_string(widgetRect.h) + ")")
-                      .c_str());
-
     bool status = eventHandler.isMouseButtonDown() && Utilities::isPointInsideRect(mousePos, widgetRect);
+
+    if (status && clickCallback != nullptr)
+    {
+        clickCallback->run(this);
+    }
+
     return status;
 }
 
